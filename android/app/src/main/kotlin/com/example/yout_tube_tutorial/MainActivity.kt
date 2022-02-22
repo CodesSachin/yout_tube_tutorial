6b9065b4b6fc758bcc3d734963334f19a095a90b
CodesSachin <71680245+CodesSachin@users.noreply.github.com>
2022-02-22 13:36:17 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/yout_tube_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yout_tube_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main(){ runApp();}
+
+d
